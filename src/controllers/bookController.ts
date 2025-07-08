56139a55024951d8399ae3ab0399d90211d3cdae
MaximoVerdi <maximoverdi21@gmail.com>
2025-07-08 17:38:59 -0300

    Funcion createBook

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -21,3 +21,12 @@ export const getBookById = async (req: Request, res: Response) => {
   }
 };
 
+export const createBook = async (req: Request, res: Response) => {
+  try {
+    const book = new Book(req.body);
+    await book.save();
+    res.status(201).json({ success: true, data: book });
+  } catch (error) {
+    res.status(400).json({ success: false, message: 'Error al crear libro' });
+  }
+};
